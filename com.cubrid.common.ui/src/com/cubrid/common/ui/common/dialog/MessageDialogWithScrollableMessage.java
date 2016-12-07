7cce6b5701c01e3c579226dce28534868199074b
Paul Gheorghe <paul.gheorghe@simpledcard.com>
2016-12-07 13:52:12 +0200

    ScrollDriversList: remove a couple of double lines

--- com.cubrid.common.ui/src/com/cubrid/common/ui/common/dialog/MessageDialogWithScrollableMessage.java
+++ com.cubrid.common.ui/src/com/cubrid/common/ui/common/dialog/MessageDialogWithScrollableMessage.java
@@ -1,6 +1,5 @@
 package com.cubrid.common.ui.common.dialog;
 
-
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
@@ -12,7 +11,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-
 
 public class MessageDialogWithScrollableMessage extends MessageDialog {
 
