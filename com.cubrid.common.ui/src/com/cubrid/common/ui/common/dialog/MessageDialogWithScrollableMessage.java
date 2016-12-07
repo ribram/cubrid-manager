a08a0815367c738dd5eb82861c72c4c00c0e2425
Paul Gheorghe <paul.gheorghe@simpledcard.com>
2016-12-07 15:44:39 +0200

    ScrollDriversList: remove redundant setLayout. format code

--- com.cubrid.common.ui/src/com/cubrid/common/ui/common/dialog/MessageDialogWithScrollableMessage.java
+++ com.cubrid.common.ui/src/com/cubrid/common/ui/common/dialog/MessageDialogWithScrollableMessage.java
@@ -6,7 +6,6 @@ import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -15,36 +14,34 @@ import org.eclipse.swt.widgets.Shell;
 public class MessageDialogWithScrollableMessage extends MessageDialog {
 
 	private String message;
-	
-    public MessageDialogWithScrollableMessage(Shell parentShell, String title, Image titleImage, String mainMessage, String secondaryMessage, int imageType, String[] buttonLabels) {
-    	super(parentShell, title, null, mainMessage,
-    			imageType, buttonLabels, 0);
-    	this.message = secondaryMessage;
-    }
 
-    @Override
-    public Control createDialogArea(Composite parent) {
-    	if(this.message.length() == 0){
-    		return super.createDialogArea(parent);
-    	}
-    	Composite content = (Composite) super.createDialogArea(parent);
-    	content.setLayout(new GridLayout());
-        GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-    	
-    	ScrolledComposite sc = new ScrolledComposite(content, SWT.H_SCROLL
-                | SWT.V_SCROLL);
+	public MessageDialogWithScrollableMessage(Shell parentShell, String title, Image titleImage, String mainMessage,
+			String secondaryMessage, int imageType, String[] buttonLabels) {
+		super(parentShell, title, null, mainMessage, imageType, buttonLabels, 0);
+		this.message = secondaryMessage;
+	}
 
-        Composite composite = new Composite(sc, SWT.NONE);
-        composite.setLayout(new FillLayout(SWT.VERTICAL));
-        Label l = new Label(composite, SWT.NONE);
-        l.setText(message);
-        
-        sc.setLayoutData(data);
-        sc.setContent(composite);
-        sc.setExpandHorizontal(true);
-        sc.setExpandVertical(true);
-        sc.setMinSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	@Override
+	public Control createDialogArea(Composite parent) {
+		if (this.message.length() == 0) {
+			return super.createDialogArea(parent);
+		}
+		Composite content = (Composite) super.createDialogArea(parent);
+		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
 
-        return parent; 
-    }
+		ScrolledComposite sc = new ScrolledComposite(content, SWT.H_SCROLL | SWT.V_SCROLL);
+
+		Composite composite = new Composite(sc, SWT.NONE);
+		composite.setLayout(new FillLayout(SWT.VERTICAL));
+		Label l = new Label(composite, SWT.NONE);
+		l.setText(message);
+
+		sc.setLayoutData(data);
+		sc.setContent(composite);
+		sc.setExpandHorizontal(true);
+		sc.setExpandVertical(true);
+		sc.setMinSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+
+		return parent;
+	}
 }
